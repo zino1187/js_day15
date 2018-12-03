021b1b39cacbfb1a82d0eb05ccf6e10de9c9a51f
zino1187 <zino1187@gmail.com>
2018-12-03 12:59:09 +0900

    no message

--- Hero.js
+++ Hero.js
@@ -1,13 +1,17 @@
 class Hero extends GameObject{
 	constructor(container,width,height,x,y,velX,velY,bg){
-		super(container,width,height,x,y,velX,velY,bg);	
+		super(container,width,height,x,y,velX,velY,bg);
 		this.g=0.05;
-	}	
+	}
 	//부모가 이미 가진 메서드를 그대로 사용하기엔 나한테 상황에
-	//맞지 않을때 해당 메서드를 나에 맞게끔 커스터마이징 할수 
+	//맞지 않을때 해당 메서드를 나에 맞게끔 커스터마이징 할수
 	//있는데, 이러한 메서드 중복 정의 기법을 오버라이드..
 	//= 업그레이드
 	tick(){
+		this.goDown();
+		this.walk();
+	}
+	goDown(){
 		this.velY+=this.g;
 		this.y = this.y+this.velY;
 
@@ -19,6 +23,13 @@ class Hero extends GameObject{
 				this.y=blockArray[i].y - this.height;//고정!!
 			}
 		}
-
+	}
+	//점프란 양수방향으로 적용되고 있는 중력을 잠시 이겨내는 행위...
+	//따라서 일시적으로 음수방향의 값을 가지게 하면 된다..
+	jump(){
+		this.velY=-4;	
+	}
+	walk(){
+		this.x+=this.velX;
 	}
 }
